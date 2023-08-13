8b7f94a8a6ddd880fd9102747eecee85d27ad13e
jonathanvra <jonathan.vra@gmail.com>
2023-08-13 16:13:05 -0500

    The fields of the Income table were changed and some reference data was added

--- src/components/TableIncome/TableIncome.jsx
+++ src/components/TableIncome/TableIncome.jsx
@@ -3,45 +3,102 @@ import { DataGrid } from '@mui/x-data-grid';
 import { Box } from '@mui/material';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'tipo_bien', headerName: 'Tipo de bien', width: 140 },
+  { field: 'articulo', headerName: 'Articulo', width: 230 },
+  { field: 'proveedor', headerName: 'Proveedor', width: 160 },
+  { field: 'cartera', headerName: 'Cartera', width: 120 },
+  { field: 'marca', headerName: 'Marca', width: 120 },
+  { field: 'referencia', headerName: 'Referencia', width: 190 },
+  { field: 'modelo', headerName: 'Modelo', width: 190 },
+  { field: 'moneda', headerName: 'Moneda', width: 90 },
+  { field: 'iva', headerName: 'IVA', width: 90 },
+  { field: 'ubicacion', headerName: 'Ubicación', width: 180 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: 'control_legalizable',
+    headerName: 'Control/Legalizable',
+    width: 140,
   },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  {
+    id: 1,
+    tipo_bien: 'Activo fijo',
+    articulo: 'Sensor de alcantarillado',
+    proveedor: 'DF ROBOT',
+    cartera: 'Riesgos',
+    marca: 'DF-ROBOT',
+    referencia: 'TF03-UART 5-24V',
+    modelo: 'TF03-UART 5-24V',
+    moneda: 'USD',
+    iva: '10%',
+    ubicacion: 'Bodega Casa SIATA',
+    control_legalizable: 'Control',
+  },
+  {
+    id: 2,
+    tipo_bien: 'Activo fijo',
+    articulo: 'Sensor de nivel',
+    proveedor: 'SIEMENS S.A.',
+    cartera: 'Riesgos',
+    marca: 'SIEMENS',
+    referencia: 'PBE-5AC-620',
+    modelo: 'PBE-5AC-620',
+    moneda: 'COP',
+    iva: '19%',
+    ubicacion: 'Bodega 808',
+    control_legalizable: 'Legalizable',
+  },
+  {
+    id: 3,
+    tipo_bien: 'Activo fijo',
+    articulo: 'Antena',
+    proveedor: 'CORE IP',
+    cartera: 'Riesgos',
+    marca: 'UBIQUITI',
+    referencia: 'MAX SECTOR 5AC 21-60',
+    modelo: 'MAX SECTOR 5AC 21-60',
+    moneda: 'COP',
+    iva: '19%',
+    ubicacion: 'Torre SIATA',
+    control_legalizable: 'Control',
+  },
+  {
+    id: 4,
+    tipo_bien: 'Activo fijo',
+    articulo: 'Antena',
+    proveedor: 'CORE IP',
+    cartera: 'Aire',
+    marca: 'UBIQUITI',
+    referencia: 'R-5AC- PRISM',
+    modelo: 'R-5AC- PRISM',
+    moneda: 'COP',
+    iva: '19%',
+    ubicacion: 'Torre SIATA',
+    control_legalizable: 'Control',
+  },
+  {
+    id: 5,
+    tipo_bien: 'Activo fijo',
+    articulo: 'Pluviometro',
+    proveedor: 'DAVIS INSTRUMENT',
+    cartera: 'Riesgos',
+    marca: 'DAVIS',
+    referencia: 'RAIN COLLECTOR II',
+    modelo: 'RAIN COLLECTOR II',
+    moneda: 'USD',
+    iva: '0%',
+    ubicacion: 'Casa SIATA',
+    control_legalizable: 'Control',
+  },
 ];
 
 export default function DataTable() {
   return (
     <Box
       sx={{
-        margin: '10px'
-      }}
-    >
+        margin: '10px',
+      }}>
       <DataGrid
         rows={rows}
         columns={columns}
@@ -50,7 +107,7 @@ export default function DataTable() {
             paginationModel: { page: 0, pageSize: 5 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[1, 3, 5]}
         checkboxSelection
       />
     </Box>
