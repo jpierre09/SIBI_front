a28f3ade1aa701fea9ad1b3642c5c41926238f2b
Jean Pierre <jeanlondono09@gmail.com>
2023-08-14 18:47:28 -0500

    FIX outflow endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ import {
   Link 
 } from 'react-router-dom';
 import { Income } from './pages/Income';
-import { Outflows } from './pages/Outflows';
 import Home from './pages/Home';
+import { Outflows } from './pages/Outflows';
 
 function App() {
   return (
     <Routes>
       <Route path="/income" element={<Income />} />
       <Route path='/' element={<Home />} />
-      <Route path='/egresos' element={<Outflows />} />
+      <Route path='/outflows' element={<Outflows />} />
     </Routes>
   );
 }
