8256a7823e46d8d20c5a988f8f8e7728bdaa4812
Jean Pierre <jeanlondono09@gmail.com>
2023-08-14 23:08:26 -0500

    MOD modal outflows

--- src/pages/OutflowsModal.jsx
+++ src/pages/OutflowsModal.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { Modal, Box, Typography, TextField, Button } from '@mui/material';
+import { Modal, Box, Typography, TextField, Button, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
 
 const OutflowsModal = ({ open, onClose }) => {
+  // Estados
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [location, setLocation] = React.useState("");
+  const [description, setDescription] = React.useState("");
+
+  const [articleData, setArticleData] = React.useState({});
+
+  // Fecha y hora actuales
+  const currentDate = new Date();
+  const today = currentDate.toISOString().split('T')[0];
+  const currentTime = currentDate.toTimeString().split(' ')[0].slice(0, 5);
+  const [selectedDate, setSelectedDate] = React.useState(today);
+  const [selectedTime, setSelectedTime] = React.useState(currentTime);
+
+  const fetchArticleData = (searchTerm) => {
+    if (searchTerm) {
+      setArticleData({
+        serial: '123456',
+        referencia: 'Ref-001',
+        tipoDeBien: 'Electrónico',
+        marca: 'Samsung',
+        modelo: 'S21'
+      });
+    } else {
+      setArticleData({});
+    }
+  }
+
   return (
     <Modal open={open} onClose={onClose}>
       <Box
         sx={{
-          width: '400px',
+          width: '800px',
           margin: 'auto',
-          marginTop: '10vh',
+          marginTop: '5vh',
           padding: '20px',
           backgroundColor: 'white',
           borderRadius: '10px',
@@ -18,24 +46,116 @@ const OutflowsModal = ({ open, onClose }) => {
         <Typography variant="h5" gutterBottom>
           Crear nuevo egreso
         </Typography>
-        <TextField
-          fullWidth
-          margin="normal"
-          variant="outlined"
-          label="Nombre del producto"
-        />
-        <TextField
-          fullWidth
-          margin="normal"
-          variant="outlined"
-          label="Cantidad"
-        />
-        <TextField
-          fullWidth
-          margin="normal"
-          variant="outlined"
-          label="Precio"
-        />
+
+        <Box sx={{ display: 'flex' }}>
+          <Box sx={{ flexGrow: 1 }}>
+            {/* Selector de fecha y hora */}
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '16px' }}>
+              <TextField
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                label="Fecha"
+                type="date"
+                defaultValue={today}
+                value={selectedDate}
+                onChange={(e) => setSelectedDate(e.target.value)}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                sx={{ marginRight: '16px', width: 'calc(50% - 8px)' }}
+              />
+
+              <TextField
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                label="Hora"
+                type="time"
+                defaultValue={currentTime}
+                value={selectedTime}
+                onChange={(e) => setSelectedTime(e.target.value)}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                sx={{ width: 'calc(50% - 8px)' }}
+              />
+            </Box>
+
+            {/* Campo de descripción */}
+            <TextField
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              label="Descripción del egreso"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              multiline
+              rows={4}
+            />
+          </Box>
+
+          <Box sx={{ marginLeft: '20px', width: '40%' }}>
+            {/* Campo de búsqueda */}
+            <TextField
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              label="Buscar artículo"
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+                fetchArticleData(e.target.value);
+              }}
+            />
+
+            {articleData.serial && (
+              <Box>
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  label="Serial"
+                  value={articleData.serial}
+                  disabled
+                />
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  label="Referencia"
+                  value={articleData.referencia}
+                  disabled
+                />
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  label="Tipo de Bien"
+                  value={articleData.tipoDeBien}
+                  disabled
+                />
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  label="Marca"
+                  value={articleData.marca}
+                  disabled
+                />
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  label="Modelo"
+                  value={articleData.modelo}
+                  disabled
+                />
+              </Box>
+            )}
+          </Box>
+        </Box>
+
         <Box
           sx={{
             display: 'flex',
@@ -54,7 +174,7 @@ const OutflowsModal = ({ open, onClose }) => {
               onClose();
             }}
           >
-            Añadir
+            Crear egreso
           </Button>
         </Box>
       </Box>
