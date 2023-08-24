f187338b9ef35cb0bb06d826367a8bdd1fbea8f5
jonathanvra <jonathan.vra@gmail.com>
2023-08-23 21:25:39 -0500

    changes login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginForm from '../components/Login/LoginSibi';
-import axios from 'axios';
+import { handleLogin } from '../services/loginApi';
 
 const LoginPage = () => {
+  const [loginError, setLoginError] = useState(null);
 
-  const [loginError, setLoginError] = React.useState(null);
-
-
-  const handleLogin = (username, password) => {
-    axios.post('http://127.0.0.1:8000/api/token/', {
-        username: username,
-        password: password
-    }, {
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    })
-    .then(response => {
-        if (response.status === 200) {
-            console.log("Inicio de sesión exitoso:", response.data);
-            localStorage.setItem('access_token', response.data.access);
-            localStorage.setItem('refresh_token', response.data.refresh);
-            window.location.href = "/";
-        }
-    })
-    .catch(error => {
-      setLoginError("Credenciales inválidas. Inténtalo de nuevo.");
-      console.error("Error en el inicio de sesión:", error.response);
-    });
+  const handleUserLogin = async (username, password) => {
+    try {
+      await handleLogin(username, password, setLoginError);
+    } catch (error) {
+      console.error('Error en el manejo del inicio de sesión.', error);
+      setLoginError(
+        'Hubo un error en el inicio de sesión. Por favor, inténtalo de nuevo.'
+      );
+    }
   };
 
   // Función para manejar el cierre de sesión
-  const handleLogout = () => {
+  const handleLogout = async () => {
     localStorage.removeItem('access_token');
     localStorage.removeItem('refresh_token');
-    window.location.href = "/login";  // Redirige al usuario a la página de inicio de sesión
-  }
+    window.location.href = '/login'; // Redirige al usuario a la página de inicio de sesión
+  };
 
   return (
     <div>
