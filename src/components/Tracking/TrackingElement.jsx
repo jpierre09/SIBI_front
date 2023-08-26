ef3de70b11f916e2c4fd96d40c0610bce973fc7b
Jean Pierre <jeanlondono09@gmail.com>
2023-08-26 17:41:52 -0500

    Tracking item notfound validation

--- src/components/Tracking/TrackingElement.jsx
+++ src/components/Tracking/TrackingElement.jsx
@@ -3,34 +3,44 @@ import { Button, Box, TextField, Typography } from '@mui/material';
 
 const Tracking = () => {
     const [searchQuery, setSearchQuery] = useState('');  // Nuevo estado para la consulta de búsqueda
+    const [notFound, setNotFound] = useState(false); // Nuevo estado para manejar si el bien no se encontró
 
     const handleSearch = () => {
         console.log('Buscar eventos para:', searchQuery);
+
+        // Simulación de una búsqueda fallida, debes reemplazar este bloque por tu llamada a API real
+        if (searchQuery !== 'encontrado') {
+            setNotFound(true);
+        } else {
+            setNotFound(false);
+        }
     };
 
     return (
         <Box display="flex" flexDirection="column" alignItems="center">
 
-            {/* Título opcional para la sección */}
             <Typography variant="h6" sx={{ marginBottom: '8px', textAlign: 'left', width: '100%' }}>
-                Tracking de bienes
+                Tracking de evento
             </Typography>
 
-            {/* Campo de búsqueda */}
             <Box display="flex" alignItems="center" sx={{ width: '100%', justifyContent: 'flex-start' }}>
                 <TextField
                     label="AMVA / serial"
                     variant="outlined"
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
-                    required  // Hace que el campo sea obligatorio
+                    required
                     sx={{ width: 'calc(10%)' }}
                 />
-
-                
             </Box>
 
-            {/* Botón para hacer tracking */}
+            {/* Mostrar mensaje si no se encontró el bien */}
+            {notFound && (
+                <Typography variant="body2" color="error" sx={{ mt: 2 }}>
+                    No se encontró el bien.
+                </Typography>
+            )}
+
             <Box display="flex" justifyContent="flex-start" width="100%">
                 <Button 
                     variant="contained" 
@@ -46,4 +56,5 @@ const Tracking = () => {
 };
 
 export default Tracking;
+
 
