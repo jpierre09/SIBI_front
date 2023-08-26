9094d023f798dd9250bdea1dc13c702bd80a5d87
Jean Pierre <jeanlondono09@gmail.com>
2023-08-26 15:51:02 -0500

    Validate Generar reporte to mandatory dates

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -1,41 +1,77 @@
-import React, { useState } from 'react';
-import { Button, Box, TextField, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Button, Box, TextField, Typography, RadioGroup, FormControlLabel, Radio, FormControl } from '@mui/material';
 
 const Reports = () => {
-    const [selectedDate, setSelectedDate] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [reportType, setReportType] = useState('ingreso');
+    const [validDateRange, setValidDateRange] = useState(true);
+
+    useEffect(() => {
+        if (startDate && endDate) {
+            const start = new Date(startDate);
+            const end = new Date(endDate);
+            const diffTime = Math.abs(end - start);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+            if (diffDays > 30) {
+                setValidDateRange(false);
+            } else {
+                setValidDateRange(true);
+            }
+        }
+    }, [startDate, endDate]);
 
     const handleGenerateReport = () => {
-        console.log('Generar reporte para la fecha:', selectedDate);
+        if (!startDate || !endDate) {
+            alert('Ambas fechas son obligatorias.');
+            return;
+        }
+        
+        if (validDateRange) {
+            console.log(`Generar reporte de tipo ${reportType} desde ${startDate} hasta ${endDate}`);
+        } else {
+            alert('El rango de fechas no puede ser mayor a un mes.');
+        }
     };
 
     return (
         <Box display="flex" flexDirection="column" alignItems="center">
+            <Box sx={{ width: '100%', justifyContent: 'flex-start', marginBottom: '8px' }}>
+                <FormControl component="fieldset">
+                    <RadioGroup row value={reportType} onChange={(e) => setReportType(e.target.value)}>
+                        <FormControlLabel value="ingreso" control={<Radio />} label="Ingresos" />
+                        <FormControlLabel value="egreso" control={<Radio />} label="Egresos" />
+                    </RadioGroup>
+                </FormControl>
+            </Box>
+            
             <Box display="flex" alignItems="center" sx={{ width: '100%', justifyContent: 'flex-start' }}>
                 <TextField
+                    required
                     fullWidth
                     margin="normal"
                     variant="outlined"
                     label="Fecha inicio"
                     type="date"
-                    value={selectedDate}
-                    onChange={(e) => setSelectedDate(e.target.value)}
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
                     InputLabelProps={{
                         shrink: true,
                     }}
                     sx={{ width: 'calc(10%)' }}
                 />
-                <Typography variant="body1" sx={{ margin:'0 8px'}}> 
+                <Typography variant="body1" sx={{ margin: '0 8px' }}> 
                     hasta
                 </Typography>
-
                 <TextField
+                    required
                     fullWidth
                     margin="normal"
                     variant="outlined"
-                    label="Fecha inicio"
+                    label="Fecha fin"
                     type="date"
-                    value={selectedDate}
-                    onChange={(e) => setSelectedDate(e.target.value)}
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
                     InputLabelProps={{
                         shrink: true,
                     }}
@@ -43,13 +79,13 @@ const Reports = () => {
                 />
             </Box>
 
-
             <Box display="flex" justifyContent="flex-start" width="100%">
                 <Button 
                     variant="contained" 
                     color="primary" 
                     onClick={handleGenerateReport}
-                    sx={{ mt: 1 }}
+                    disabled={!validDateRange}
+                    sx={{ mt: 4 }}
                 >
                     Generar reporte
                 </Button>
@@ -59,4 +95,6 @@ const Reports = () => {
 };
 
 export default Reports;
+
+
 
